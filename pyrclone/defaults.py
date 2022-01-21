fabf3c4fd2091a462ce09f829eb3ec1a9cca69ae
nobody <23215696+taicaile@users.noreply.github.com>
2022-01-21 15:02:16 +0800

    rename cwd to basedir

--- pyrclone/defaults.py
+++ pyrclone/defaults.py
@@ -1,5 +1,5 @@
 """defaults"""
 RCLONE_MOVE_KEY_PREFIX = "rclone-move"
-RCLONE_MOVE_KEY_PATTERN = "{prefix}:{remote}:{cwd}"
+RCLONE_MOVE_KEY_PATTERN = "{prefix}:{remote}:{basedir}"
 
 REDIS_URL = "redis://localhost"
--- pyrclone/move.py
+++ pyrclone/move.py
@@ -14,29 +14,29 @@ logger = logging.getLogger()
 class RcloneMoveProducer:
     """rclone move"""
 
-    def __init__(self, key, queue, cwd) -> None:
+    def __init__(self, key, queue, basedir) -> None:
 
         self.key = key
-        self.cwd = cwd
+        self.basedir = basedir
         self.queue = queue
 
     @classmethod
     def from_settings(cls, settings):
         """from settings"""
-        cwd = os.path.abspath(settings.get("CWD", None) or os.getcwd())
+        basedir = os.path.abspath(settings.get("BASEDIR", None) or os.getcwd())
         remote = settings.get("RCLONE_REMOTE")
         prefix = settings.get("RCLONE_MOVE_KEY_PREFIX", defaults.RCLONE_MOVE_KEY_PREFIX)
         pattern = settings.get(
             "RCLONE_MOVE_KEY_PATTERN", defaults.RCLONE_MOVE_KEY_PATTERN
         )
-        key = pattern.format(remote=remote, prefix=prefix, cwd=cwd)
+        key = pattern.format(remote=remote, prefix=prefix, basedir=basedir)
         queue = RedisQueue.from_settings(settings)
 
-        return cls(key=key, queue=queue, cwd=cwd)
+        return cls(key=key, queue=queue, basedir=basedir)
 
     def relative(self, filepath: str):
         if os.path.isabs(filepath):
-            filepath = os.path.relpath(filepath, self.cwd)
+            filepath = os.path.relpath(filepath, self.basedir)
         return filepath
 
     def push(self, filepath):
